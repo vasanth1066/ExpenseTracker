8d8945f491011f03e4dfa9121331001ce4f72ea8
vasanth1066 <vasanthanuvavi@gmail.com>
2024-04-14 00:42:55 +0530

    Forgot Password Workflow

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./Components/Pages/Login";
 import SignIn from "./Components/Pages/SignIn";
 import ExpenseTracker from "./Components/Pages/ExpenseTracker";
 import Completeprofile from "./Components/Pages/CompleteProfile";
+import ForgotPassword from "./Components/Pages/Forgotpassword";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path="/SignIn" element={<SignIn />} />
       <Route path="/ExpenseTracker" element={<ExpenseTracker />} />
       <Route path="/Completeprofile" element={<Completeprofile />} />
+      <Route path="/ForgotPassword" element={<ForgotPassword />} />
     </Routes>
   );
 }
--- src/Components/Pages/Forgotpassword.js
+++ src/Components/Pages/Forgotpassword.js
@@ -2,24 +2,24 @@ import React, { useRef } from "react";
 import MainHeader from "../Header/MainHeader";
 import { useNavigate } from "react-router-dom";
 
-const SignIn = () => {
+const ForgotPassword = () => {
   const emailinputref = useRef();
-  const passwordinputref = useRef();
-  const Navigate = useNavigate();
-
-  const submitHandler = (event) => {
+  const navigate = useNavigate();
+  const handleLoginClick = (event) => {
     event.preventDefault();
+    navigate("/SignIn");
+  };
+  const submitHandler = (event) => {
     const useremail = emailinputref.current.value;
-    const userpassword = passwordinputref.current.value;
 
+    event.preventDefault();
     fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB7DSTo_2-kms1kJYEyWucRdwmYUTmNZHo",
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyB7DSTo_2-kms1kJYEyWucRdwmYUTmNZHo",
       {
         method: "POST",
         body: JSON.stringify({
+          requestType: "PASSWORD_RESET",
           email: useremail,
-          password: userpassword,
-          returnSecureToken: true,
         }),
         headers: {
           "Content-Type": "application/json",
@@ -41,32 +41,28 @@ const SignIn = () => {
         }
       })
       .then((data) => {
-        console.log("Logged in", data.idToken);
-        localStorage.setItem("token", data.idToken);
-        Navigate("/ExpenseTracker");
+        console.log("forgotpassword page---", data);
       })
       .catch((err) => {
         alert(err.message);
       });
   };
-
-  const handleLoginClick = () => {
-    Navigate("/Login");
-  };
   return (
-    <div>
+    <>
       <MainHeader />
       <div
-        className="container-xl"
+        className="container"
         style={{
           position: "fixed",
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
           backgroundSize: "cover",
+          backgroundImage:
+            "url('https://img.freepik.com/free-vector/white-wave-white-background-with-blue-lines_483537-4174.jpg')",
         }}
       >
-        <h2>SignIn</h2>
+        <h2>Forgot Password</h2>
         <form onSubmit={submitHandler}>
           <div className="mb-7 col-4">
             <label htmlFor="exampleInputEmail1" className="form-label">
@@ -81,37 +77,22 @@ const SignIn = () => {
               required
             />
           </div>
-          <div className="mb-7 col-4">
-            <label htmlFor="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              ref={passwordinputref}
-              className="form-control"
-              id="exampleInputPassword1"
-              required
-            />
-          </div>
+
           <br />
 
-          <button type="submit" className="  btn btn-primary">
-            Login
+          <button type="submit" className="  btn btn-secondary">
+            Submit
           </button>
         </form>
-        <h1></h1>
-        <a href="/ForgotPassword" class="link-danger">
-          Forgot Password
-        </a>
         <br />
         <br />
 
-        <span className=" mb-7 col-4  ">Create an Account </span>
-        <button onClick={handleLoginClick} className="  btn btn-secondary ">
-          Go to Signup
+        <button onClick={handleLoginClick} className="  btn btn-success ">
+          Go to login
         </button>
       </div>
-    </div>
+    </>
   );
 };
-export default SignIn;
+
+export default ForgotPassword;
