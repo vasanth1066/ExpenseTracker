bb15cff32d5e71693077361394a5d85144d7bdcc
vasanth1066 <vasanthanuvavi@gmail.com>
2024-04-18 17:14:50 +0530

    Adding Expenses for tracking purpose

--- src/Components/Pages/ExpenseTracker.js
+++ src/Components/Pages/ExpenseTracker.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const ExpenseTracker = () => {
+  const [data, setData] = useState([]);
+  const moneyinputref = useRef();
+  const Descriptioninputref = useRef();
+  const Categoryinputref = useRef();
   const verifymailid = (event) => {
     event.preventDefault();
     const token = localStorage.getItem("token");
@@ -47,6 +51,16 @@ const ExpenseTracker = () => {
     navigate("/SignIn");
     localStorage.removeItem("token");
   };
+
+  const moneyspenthandler = (event) => {
+    event.preventDefault();
+    const usermoney = moneyinputref.current.value;
+    const userdescription = Descriptioninputref.current.value;
+    const usercategory = Categoryinputref.current.value;
+
+    setData([...data, { usermoney, userdescription, usercategory }]);
+  };
+
   return (
     <>
       <div>
@@ -73,10 +87,79 @@ const ExpenseTracker = () => {
         </nav>
       </div>
       <div>
-        <br /> <br />
+        <br />
         <button type="submit" class="btn btn-secondary" onClick={verifymailid}>
-          verify Email
+          Verify Email
         </button>
+      </div>
+      <div className="container-xl d-flex justify-content-center align-items-center">
+        <form onSubmit={moneyspenthandler}>
+          <div class="mb-7 ">
+            <label for="MoneySpent" class="form-label">
+              Money Spent
+            </label>
+            <input
+              type="number"
+              class="form-control"
+              id="MoneySpent"
+              ref={moneyinputref}
+            />
+          </div>
+          <div class="mb-7 ">
+            <label for="Description" class="form-label">
+              Description
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="Description"
+              ref={Descriptioninputref}
+            />
+          </div>
+          <div class="mb-7 ">
+            <label for="Category" class="form-label">
+              Category
+            </label>
+            <select
+              class="form-select"
+              aria-label="Default select example"
+              ref={Categoryinputref}
+            >
+              <option selected>Select any option below</option>
+              <option value="Food">Food</option>
+              <option value="Petrol">Petrol</option>
+              <option value="Movie">Movie</option>
+              <option value="Salary">Salary</option>
+            </select>
+          </div>
+          <br />
+
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+      <div className="container-xxl d-flex justify-content-left align-items-center">
+        {console.log("data", data)}
+        <div>
+          {data.map((val, index) => (
+            <ul key={index} class="list-group">
+              <li class="list-group-item d-flex justify-content-between align-items-center ">
+                <span class="list-group-item list-group-item-action list-group-item-success">
+                  Description:{val.userdescription}
+                </span>
+                <div style={{ marginRight: "10px" }}></div>
+
+                <span class="list-group-item list-group-item-action list-group-item-info">
+                  Category: {val.usercategory}
+                </span>
+                <span class="badge bg-dark rounded-pill">
+                  Money Spent: {val.usermoney}
+                </span>
+              </li>
+            </ul>
+          ))}
+        </div>
       </div>
     </>
   );
